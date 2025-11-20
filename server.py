55bc5a889b9215a6dd0dd5c767baaae798bb275c
CiscG <fgss98@gmail.com>
2025-11-19 21:01:10 -0300

    ajuste flask

--- server.py
+++ server.py
@@ -4,23 +4,23 @@ app = Flask(__name__, template_folder="páginasHTML")
 
 @app.route("/")
 def pagina1():
-    return render_template("pagina1.html")
+    return render_template("main.html")
 
 @app.route("/etapa2")
 def pagina2():
-    return render_template("pagina2.html")
+    return render_template("pagina1.html")
 
 @app.route("/etapa3")
 def pagina3():
-    return render_template("pagina3.html")
+    return render_template("pagina2.html")
 
 @app.route("/etapa4")
 def pagina4():
-    return render_template("pagina4.html")
+    return render_template("pagina3.html")
 
 @app.route("/etapa5")
 def pagina5():
-    return render_template("pagina5.html")
+    return render_template("pagina4.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- server_adhoc.py
+++ server_adhoc.py
@@ -4,23 +4,23 @@ app = Flask(__name__, template_folder="páginasHTML")
 
 @app.route("/")
 def homepage():
-    return render_template("pagina1.html")
+    return render_template("main.html")
 
 @app.route("/etapa2")
 def etapa2():
-    return render_template("pagina2.html")
+    return render_template("pagina1.html")
 
 @app.route("/etapa3")
 def etapa3():
-    return render_template("pagina3.html")
+    return render_template("pagina2.html")
 
 @app.route("/etapa4")
 def etapa4():
-    return render_template("pagina4.html")
+    return render_template("pagina3.html")
 
 @app.route("/etapa5")
 def etapa5():
-    return render_template("pagina5.html")
+    return render_template("pagina4.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8443, ssl_context="adhoc")
